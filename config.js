305edaf135d25f146d60552f05f60c179bd31be5
Botisha01 <webripnokiye76@gmail.com>
2024-12-02 18:39:24 +0530

    💜️ᴄᴏᴅᴇXʀᴜᴡᴀ-ᴠ1💜️

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=um5XCYbD#kaxhYGNUDQbsFJbjyC87y1dNFNu8MCSYf_X8QnuhONY", //put you session id
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=eigSARpZ#q1tpT74vUUbBuNo2FjEv1egNWeuKpCtf3JXWjIozSzY", //put you session id
 MONGODB: process.env.MONGODB || "mongodb://mongo:yqzUFafwPiAfqersdkRLcGAeDdYzbvuw@junction.proxy.rlwy.net:59749", //dont change
 READ_MESSAGE: process.env.READ_MESSAGE || "true",
 AUTO_REACT: process.env.AUTO_REACT || "true",
